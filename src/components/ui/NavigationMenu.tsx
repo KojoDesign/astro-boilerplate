babb359516dacb006ef1c7fdc1f046fe1cfabed6
Tyler Nickerson <nickersoft@gmail.com>
2025-01-14 11:03:20 -0500

    fix: update NavigationMenu API

--- src/components/ui/NavigationMenu.tsx
+++ src/components/ui/NavigationMenu.tsx
@@ -3,20 +3,20 @@ import { Menu } from "@/icons";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/Sheet";
 
 interface NavigationMenuProps {
-  desktop?: React.ReactNode;
   mobile?: React.ReactNode;
+  children: React.ReactNode;
 }
 
-export function NavigationMenu({ desktop, mobile }: NavigationMenuProps) {
+export function NavigationMenu({ children, mobile }: NavigationMenuProps) {
   return (
     <>
-      <div className="contents laptop:hidden">{desktop}</div>
+      <div className="contents laptop:hidden">{children}</div>
       <div className="hidden laptop:contents">
         <Sheet>
           <SheetTrigger className="flex justify-center items-center">
             <Menu className="size-6" />
           </SheetTrigger>
-          <SheetContent>{mobile}</SheetContent>
+          <SheetContent>{mobile ?? children}</SheetContent>
         </Sheet>
       </div>
     </>
